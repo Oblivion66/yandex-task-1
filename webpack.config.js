922116de894bac59837a2a869344a404a8834980
unknown <myradusmanov@gmail.com>
2024-10-06 21:34:49 +0300

    djjd

--- webpack.config.js
+++ webpack.config.js
@@ -70,11 +70,11 @@ module.exports = {
           "css-loader",
         ],
       },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      }
+    //   {
+    //     test: /\.js$/,
+    //     exclude: /node_modules/,
+    //     use: ['babel-loader'],
+    //   }
     ],
   },
 };
